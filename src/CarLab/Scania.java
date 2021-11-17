531eb0194fe22b4c4b1f3c1190331a42b882cb92
Julia <juliakaufmann@live.se>
2021-11-17 15:50:56 +0100

    Add Scania class

---
+++ src/CarLab/Scania.java
@@ -0,0 +1,12 @@
+package CarLab;
+
+public class Scania extends Car{
+
+    public Scania() {
+        super();
+    }
+    @Override
+    protected double speedFactor() {
+        return 0;
+    }
+}
