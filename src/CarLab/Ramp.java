15510f2361dc9304775f6031c12dfa74f99f8690
WilliamBoomer <willjoha@net.chalmers.se>
2021-11-24 11:48:29 +0100

    Added load and unload methods to Scania

--- src/CarLab/Ramp.java
+++ src/CarLab/Ramp.java
@@ -3,13 +3,10 @@ package CarLab;
 import java.util.Stack;
 
 public class Ramp {
-    private final int capacity;
     private final double maxRampAngle;
     private double rampAngle;
-    private final Stack<Car> cars = new Stack<>();
 
     public Ramp(int capacity, double maxRampAngle) {
-        this.capacity = capacity;
         this.rampAngle = 0;
         this.maxRampAngle = maxRampAngle;
     }
@@ -33,50 +30,11 @@ public class Ramp {
         rampAngle = Math.max(getRampAngle() - amount, 0);
     }
 
-    /**
-     * A getter for how many cars there are in the stack numberOfCars.
-     * @return Returns int value from numberOfCars.
-     */
-    public int getNumberOfCars() {
-        return cars.size();
-    }
-
-    /**
-     * Load car method adds a car object to the top of the cars stack if the ramp angle is close or equal
-     * to the max ramp angle the and the ramp's capacity.
-     * @param car The Car object to be added to the cars stack.
-     * @return boolean if the action succeeded and the car could be loaded.
-     */
-    public boolean loadCar(Car car) {
-        if (getRampAngle() > maxRampAngle-5 && cars.size() < capacity) {
-            cars.add(car);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * Un load car method removes the car on the top of the cars stack, if the cars stack is empty then an exception is throw.n
-     * @return Returns the car at the top of the cars stack if there is one.
-     * @throws ArrayIndexOutOfBoundsException
-     */
-    public Car unLoadCar() throws ArrayIndexOutOfBoundsException {
-        if (cars.size() == 0) {
-            throw new ArrayIndexOutOfBoundsException("There is no cars on the ramp to unload.");
-        }
-        return cars.pop();
-    }
-
-    /**
+     /**
      * Getter for the attribute rampAngle
      * @return Returns double value from rampAngle
      */
     public double getRampAngle() {
         return rampAngle;
-    }
-
-    public Stack<Car> getCars() {
-        return cars;
     }
 }
