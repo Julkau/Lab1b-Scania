d198c2c7a49f74f0b8318186e1a7630542f0297b
Julia <juliakaufmann@live.se>
2021-11-25 14:45:48 +0100

    Add to workshop exception

--- src/CarLab/Workshop.java
+++ src/CarLab/Workshop.java
@@ -2,6 +2,7 @@ package CarLab;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Workshop<T extends Car> {
     private final int maxCapacity;
@@ -26,7 +27,7 @@ public class Workshop<T extends Car> {
         }
     }
 
-    public void getCar(T car) {
+    public void getCar(T car) throws NoSuchElementException {
         for (int i = 0; i<currentCapacity; i++){
             if (cars.get(i).getLicensePlate().equals(car.getLicensePlate())){
                 cars.remove(i);
@@ -34,5 +35,6 @@ public class Workshop<T extends Car> {
                 break;
             }
         }
+        throw new NoSuchElementException("No car with matching licence plate could be found. ");
     }
 }
