3c5073ad6f339cf11155a490e2838f1e4ca7b5e1
Julia <juliakaufmann@live.se>
2021-11-25 14:52:38 +0100

    Add to workshop comments

--- src/CarLab/Workshop.java
+++ src/CarLab/Workshop.java
@@ -4,19 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * A workshop for cars to be stored in. The cars can also be collected again.
+ * @param <T> extends the Car-type and turns the workshop to a specific car-typ workshop.
+ */
 public class Workshop<T extends Car> {
     private final int maxCapacity;
     private int currentCapacity;
     private List<T> cars = new ArrayList<>();
 
+    /**
+     * Creates a car specific workshop with a max capacity.
+     * @param maxCapacity is the max integer amount of cars to be stored in the workshop.
+     */
     public Workshop(int maxCapacity) {
         this.maxCapacity = maxCapacity;
         this.currentCapacity = 0;
     }
 
+    /**
+     * Gets the current capacaty of cars in the workshop.
+     * @return an int of the number of cars.
+     */
     public int getCurrentCapacity() {
         return currentCapacity;
     }
+
+    /**
+     * Stores a Car matching the workshop parameter in the workshop. If max capacity is reached the car will not be
+     * stored and an exception will be thrown.
+     * @param car is the car to be stored.
+     * @throws ArrayStoreException
+     */
 
     public void storeCar(T car) throws ArrayStoreException{
         if(currentCapacity < maxCapacity) {
@@ -27,6 +46,11 @@ public class Workshop<T extends Car> {
         }
     }
 
+    /**
+     * Gets the matching car. Looks for the same licensePlate.
+     * @param car is the matching car to search for.
+     * @throws NoSuchElementException
+     */
     public void getCar(T car) throws NoSuchElementException {
         for (int i = 0; i<currentCapacity; i++){
             if (cars.get(i).getLicensePlate().equals(car.getLicensePlate())){
@@ -35,6 +59,6 @@ public class Workshop<T extends Car> {
                 break;
             }
         }
-        throw new NoSuchElementException("No car with matching licence plate could be found. ");
+        throw new NoSuchElementException("No car with matching licence plate could be found.");
     }
 }
